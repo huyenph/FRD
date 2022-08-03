298e198b9c6fee6142f9f2a00296a9604a2993f9
HuyenPham <huyen.phamhoang96@gmail.com>
2022-08-03 15:40:07 +0700

    Update Video player

--- lib/modules/player/video_player_screen.dart
+++ lib/modules/player/video_player_screen.dart
@@ -40,8 +40,10 @@ class _VideoPlayerScreenState extends BaseState<VideoPlayerScreen> {
   PreferredSizeWidget? buildAppBar() => null;
 
   void initPlayer() async {
-    _networkController = VideoPlayerController.network(
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+    _networkController = VideoPlayerController.asset(
+      'assets/videos/packages_video_player_video_player_example_assets_Butterfly-209.webm',
+      closedCaptionFile: _loadCaption(),
+      videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
     );
     // _networkController.setLooping(true);
     // await _networkController.initialize();
@@ -51,6 +53,42 @@ class _VideoPlayerScreenState extends BaseState<VideoPlayerScreen> {
         looping: true,
         aspectRatio: 16 / 9,
         autoInitialize: true,
+        // allowFullScreen: true,
+        // subtitle: Subtitles([
+        //   Subtitle(
+        //     index: 0,
+        //     start: Duration.zero,
+        //     end: const Duration(seconds: 3),
+        //     text: 'Hello from subtitles',
+        //   ),
+        //   Subtitle(
+        //     index: 1,
+        //     start: const Duration(seconds: 4),
+        //     end: const Duration(seconds: 7),
+        //     text: 'Whats up? :)',
+        //   ),
+        // ]),
+        // subtitleBuilder: (context, subtitle) => Container(
+        //       padding: const EdgeInsets.all(10.0),
+        //       child: Text(
+        //         subtitle,
+        //         style: const TextStyle(color: Colors.white),
+        //       ),
+        //     ),
+        additionalOptions: (context) {
+          return <OptionItem>[
+            OptionItem(
+              onTap: () => debugPrint('My option works!'),
+              iconData: Icons.chat,
+              title: 'My localized title',
+            ),
+            OptionItem(
+              onTap: () => debugPrint('Another option working!'),
+              iconData: Icons.chat,
+              title: 'Another localized title',
+            ),
+          ];
+        },
         errorBuilder: (context, errorMessage) {
           return Center(
             child: UpmText(
@@ -66,7 +104,7 @@ class _VideoPlayerScreenState extends BaseState<VideoPlayerScreen> {
 
   Future<ClosedCaptionFile> _loadCaption() async {
     final String fileContents = await DefaultAssetBundle.of(context).loadString(
-      'assets/bumble_bee_caption.vtt',
+      'assets/captions/bumble_bee_captions.vtt',
     );
     return WebVTTCaptionFile(fileContents);
   }
