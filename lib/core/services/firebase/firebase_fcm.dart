8a8632feac5a00dd9a2b92721a814cf847bf9ca9
HuyenPham <huyen.phamhoang96@gmail.com>
2023-08-09 16:26:06 +0700

    Refactor service directory

--- lib/core/services/firebase/firebase_fcm.dart
+++ lib/core/services/firebase/firebase_fcm.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hpcompose/core/navigation/navigation_service.dart';
 import 'package:hpcompose/di/injector_setup.dart';
